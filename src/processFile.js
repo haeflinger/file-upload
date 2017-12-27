ae3925b0744fef5657f2e1783477dbe3698b7a3e
Kurt Haeflinger <haeflinger.kurt@mayo.edu>
2017-12-27 14:05:41 -0600

    remove test files and update .git ignore

---
+++ src/processFile.js
@@ -0,0 +1,34 @@
+import XLSX from 'xlsx';
+
+function processFile(acceptedFiles) {
+    acceptedFiles.forEach(file => {
+        const reader = new FileReader();
+        reader.onload = (e) => {
+            /* Parse data */
+            const bstr = e.target.result;
+            const wb = XLSX.read(bstr, {type:'binary'});
+            /* Get first worksheet */
+            const wsname = wb.SheetNames[0];
+            const ws = wb.Sheets[wsname];
+            /* Convert array of arrays */
+            const data = XLSX.utils.sheet_to_json(ws, {header:1});
+            /* Update state */
+            this.setState({ data: data, cols: make_cols(ws['!ref']), fileName: file.name });
+        };
+        reader.onabort = () => console.log('file reading was aborted');
+        reader.onerror = () => console.log('file reading has failed');
+
+        reader.readAsBinaryString(file);
+    })
+};
+
+function make_cols(refstr/*:string*/) {
+    var o = [];
+    var range = XLSX.utils.decode_range(refstr);
+    for(var i = 0; i <= range.e.c; ++i) {
+      o.push({name: XLSX.utils.encode_col(i), key:i});
+    }
+    return o;
+  }
+
+export default processFile;
